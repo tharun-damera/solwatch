d2ef532ba83e51f0d359a7a1104df9477413ca37
Tharun <tharun.damera4@gmail.com>
2025-12-04 23:56:21 +0530

    Fix the null as input value error

--- frontend/src/components/SearchBox.jsx
+++ frontend/src/components/SearchBox.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export default function SearchBox({ loading, setAddress, onSearch }) {
-  let [addr, setAddr] = useState(null);
+  let [addr, setAddr] = useState("");
 
   function onSubmit(e) {
     e.preventDefault();
